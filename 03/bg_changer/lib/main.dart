91f0d5e322d83b34710a7ab9e0854347ce7d8069
MihirJadhav00 <jadhavmihir143@gmail.com>
2020-09-17 23:33:16 +0530

    Started BG Changer Application

---
+++ 03/bg_changer/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BG Changer',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Random Background"),
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
