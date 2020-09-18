d6696901715325abe9dddf6722f837059c2219d5
MihirJadhav00 <jadhavmihir143@gmail.com>
2020-09-18 23:05:06 +0530

    BG Changer Application Done on a button click

--- 03/bg_changer/lib/main.dart
+++ 03/bg_changer/lib/main.dart
@@ -19,3 +19,58 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var colors = [
+    Colors.amber,
+    Colors.black,
+    Colors.blue,
+    Colors.yellow,
+    Colors.green,
+    Colors.red,
+    Colors.pink,
+    Colors.purpleAccent,
+    Colors.orange,
+    Color(0xFFF62096)
+  ];
+
+  var currentColor = Colors.white;
+  var buttonColor = Colors.black;
+
+  setRandomColor() {
+    var rnd1 = Random().nextInt(colors.length);
+    var rnd2 = Random().nextInt(colors.length);
+    setState(() {
+      currentColor = colors[rnd1];
+      buttonColor = colors[rnd2];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: currentColor,
+      child: Center(
+        child: RaisedButton(
+            color: buttonColor,
+            padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            child: Text(
+              'Change Me!',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: setRandomColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            )),
+      ),
+    );
+  }
+}
