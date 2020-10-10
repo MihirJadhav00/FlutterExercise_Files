7b5c08a3cf72502714d8ee126b0f4b6ce709ded2
MihirJadhav00 <jadhavmihir143@gmail.com>
2020-10-10 22:45:23 +0530

    06 and 07 projects done in flutter

---
+++ 07/spanishnumberaudio/lib/NumberAudio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//Helper Class
+
+class NumberAudio {
+  String audioUri;
+  MaterialColor buttonColor;
+  String buttonText;
+
+  NumberAudio(String aU, MaterialColor bC, String bT) {
+    this.audioUri = aU;
+    this.buttonColor = bC;
+    this.buttonText = bT;
+  }
+}
